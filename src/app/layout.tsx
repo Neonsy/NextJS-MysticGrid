3d8e62bf83317055fa9d0a169854163c0f9c5895
Neonsy <neonsy@users.noreply.github.com>
2024-11-19 13:46:43 +0100

    Changed font to inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from 'next';
 
+import { Inter } from 'next/font/google';
+
 import './globals.css';
+
+const inter = Inter({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 export const metadata: Metadata = {
     title: 'Mystic Grid',
@@ -17,7 +24,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={`antialiased`}>{children}</body>
+            <body className={`${inter.className} antialiased`}>{children}</body>
         </html>
     );
 }
