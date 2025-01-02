e234619a3eaed62fb87b213e2e67e6665eea50d0
Neonsy <neonsy@users.noreply.github.com>
2025-01-02 06:58:32 +0100

    Removing example vars

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -6,9 +6,6 @@ export default function Home() {
         <>
             <h1>MysticGrid</h1>
             <p>W.I.P</p>
-
-            <p>myVariable1: {myVariable1}</p>
-            <p>myVariable2: {myVariable2}</p>
         </>
     );
 }
