e6734e23aa0f2012afd05b8d38c1d3a7b1cf9b01
Neonsy <neonsy@users.noreply.github.com>
2025-01-02 06:21:24 +0100

    Vercel Deploy Action
    First draft

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
+    const myVariable1 = process.env.FOO;
+    const myVariable2 = process.env.SOME_TOKEN;
+
     return (
         <>
             <h1>MysticGrid</h1>
             <p>W.I.P</p>
+
+            <p>myVariable1: {myVariable1}</p>
+            <p>myVariable2: {myVariable2}</p>
         </>
     );
 }
