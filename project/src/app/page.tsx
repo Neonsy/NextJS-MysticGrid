1eb2d20e8d8a6c2e18a66b2f4ec2e25c134f3870
Neonsy <neonsy@users.noreply.github.com>
2025-01-03 14:23:13 +0100

    Metadata Changes Added AI Disclaimer

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        template: 'Mystic Grid | %s',
+        default: 'Mystic Grid | Placeholder',
+        absolute: 'Mystic Grid | Home',
+    },
+};
+
 export default function Home() {
     return (
         <>
