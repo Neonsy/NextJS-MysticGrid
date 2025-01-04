403ca19c66bb9afa7bdcff889b8ca4545acf0b0a
Neonsy <neonsy@users.noreply.github.com>
2025-01-04 18:19:41 +0100

    Seperated Link Logic

--- project/src/components/links/index.tsx
+++ project/src/components/links/index.tsx
@@ -1,23 +1,17 @@
-'use client';
+import { NavLinkType, ExternalNavLinkType } from '@/types/navLink';
+import ExternalLinks from '@/components/links/external';
+import InternalLinks from '@/components/links/internal';
 
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
-import Icon from '@/components/icons';
-import type { NavLinkType } from '@/types/navLink';
+type Props = {
+    links: NavLinkType[];
+};
 
-export default function Links({ links }: { links: NavLinkType[] }) {
-    const path = usePathname();
+export default function Links({ links }: Props) {
     return (
         <ul className='flex items-center gap-x-9'>
             {links.map((link) => (
                 <li key={link.text}>
-                    <Link
-                        href={link.href}
-                        target={link.target}
-                        rel={link.rel}
-                        className={`capitalize ${path === link.href ? 'font-bold' : 'hover:underline'}`}>
-                        {link.iconName ? <Icon name={link.iconName} className='h-5 w-5' /> : link.text}
-                    </Link>
+                    {(link as ExternalNavLinkType).iconName ? <ExternalLinks link={link as ExternalNavLinkType} /> : <InternalLinks link={link} />}
                 </li>
             ))}
         </ul>
