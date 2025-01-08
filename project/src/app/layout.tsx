867fda568568f5545416b42f8f8c43638a7e9048
Neonsy <neonsy@users.noreply.github.com>
2025-01-08 01:32:03 +0100

    Moved css file

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
 
-import './globals.css';
+import '@/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
